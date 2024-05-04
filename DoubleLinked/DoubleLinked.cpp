de5a4cac52606a2f0a1d88c0a66436b67fe4894b
Mihaly24 <fairuzk20@gmail.com>
2024-05-04 09:54:27 +0700

    Menambahkan step 1a-1e

--- DoubleLinked/DoubleLinked.cpp
+++ DoubleLinked/DoubleLinked.cpp
@@ -36,7 +36,18 @@ void addNode()
 		newNode->prev = NULL;
 		START = newNode;
 	}
+	else
+	{
+		Node* current = START;
+		Node* previous = NULL;
 
+		while (current != NULL && current->noMhs < newNode->noMhs)
+		{
+			previous = current;
+			current = current->next;
+		}
+
+	}
 }
 int main()
 {
